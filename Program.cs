6aaf2d55163c9bc7ad73f359c51061560d848f72
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-07-12 08:33:02 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
