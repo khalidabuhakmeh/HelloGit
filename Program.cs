92e318eb87d242fabb2a2e4c031514a7b078c4da
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-07-12 08:47:44 -0400

    added "Hello Moon"

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Moon!");
         }
     }
 }
